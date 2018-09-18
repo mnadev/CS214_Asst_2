48661b192f3085151bf4ab7b529e7ae067ab1b3e
Yukirilin <43383937+Yukirilin@users.noreply.github.com>
2018-09-18 16:04:35 -0400

    Update simpleCSVsorter.h

---
+++ simpleCSVsorter.h
@@ -0,0 +1,45 @@
+/*****
+*
+*	Define structures and function prototypes for your sorter
+*
+*
+*
+******/
+
+//Suggestion: define a struct that mirrors a record (row) of the data set
+
+typedef
+struct _movieInfo{
+	char* color;
+	char* director_name;
+	char* num_critic_for_reviews;
+	int duration;
+	char* director_facebook_likes;
+	int actor_3_facebook_likes;
+	char* actor_2_name;
+	int actor_1_facebook_likes;
+	double gross;
+	char* genres;  				
+	char* actor_1_name;
+	char* movie_title;
+	int num_voted_users;
+	int cast_total_facebook_likes;
+	char* actor_3_name;
+	int facenumber_in_poster;
+	char* plot_keywords;			
+	char* movie_imdb_link;
+	int num_user_for_reviews;
+	char* language;
+	char* country;
+	char* content_rating;
+	u_int budget;
+	int title_year;
+	int actor_2_facebook_likes;
+	double imdb_score;
+	double aspect_ratio;
+	int movie_facebook_likes;
+
+} movieInfo;
+
+//Suggestion: prototype a mergesort function
+
