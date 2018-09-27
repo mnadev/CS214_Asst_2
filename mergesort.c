d857ff190b980f08d2aafa938f8facea52508b0e
Mohammad Nadeem <mohammednadeem902@gmail.com>
2018-09-26 21:29:47 -0400

    Update mergesort.c
    removed unnecessary code

--- mergesort.c
+++ mergesort.c
@@ -5,7 +5,7 @@
 #include <ctype.h>
 #include "simpleCSVsorter.h"
 
-movieInfo** mergesort(movieInfo** arr, int size) {
+movieInfo** mergesort(movieInfo** arr, int size, int isInt) {
 	//base case of merge sort
         if(size == 1 || size == 0) {
 		return arr;
@@ -36,7 +36,7 @@ movieInfo** mergesort(movieInfo** arr, int size) {
 	
 	
 	// merge the sorted arrays
-	arr = merge(firstHalf, secondHalf, half, size-half);
+	arr = merge(firstHalf, secondHalf, half, size-half, isInt);
 	free(firstHalf);
 	free(secondHalf);
 	return arr;
@@ -52,64 +52,6 @@ int intComparator(float* intA, float* intB) {
 movieInfo** merge(movieInfo** A, movieInfo** B, int sizeA, int sizeB, int isInt){
         //index pointers
         int i = 0, j = 0;
-       
-	// this integer acts as a boolean
-	// is 0 if the data is not an int, now is 1 if the data is an int
-	int isInt = 1;
-	
-	//create temp pointer to iterate through all of array and check if each item is a number
-	//because it is possible for a movie to be a number, e.g. '300', and that would be a problem
-	//so we should check all of them to be sure
-	movieInfo** tempPtrCheckInt = A;
-	while(i < sizeA) {
-		//iterate through each char, checking if it is int
-		// NOTENOTENOTE: if you can find a better way, lmk cause this is very inefficient	
-		char* currData = (**(tempPtrCheckInt)).toBeSorted;	
-		
-		while(currData != '\0') {
-			// current char could be int or decimal point
-			if(isdigit(currData) != 0 ||  *currData == '.' ) {
-				isInt = 0;
-				break;			
-			}
-		}
-		
-		//break if we found alphabetic char, no need to check anymore
-		if(isInt == 0) {
-			break;
-		}
-
-		tempPtrCheckInt++;
-		i++;
-	}
-
-	i = 0;
-	movieInfo** tempPtrCheckInt = B;
-
-	if(isInt == 1) {
-		while(i < sizeB) {
-                	//iterate through each char, checking if it is int
-                	// NOTENOTENOTE: if you can find a better way, lmk cause this is very inefficient       
-                        char* currData = (**(tempPtrCheckInt)).toBeSorted;
-                
-                        while(currData != '\0') {
-                
-                        	if(isdigit(currData) != 0 ||  *currData == '.' ) {
-                                	isInt = 0;
-                                        break;
-                                }
-                        }
-                                                                                                                                                                                                             //break if we found alphabetic char, no need to check anymore
-                                                                                                                                                                                                             if(isInt == 0) {
-                 		break;                                                                                                                                                                       }
-                
-                        tempPtrCheckInt++;
-                        i++;
-                                                                                                                                                                                     		     }
-                      
-	}
-	
-	i = 0;
 
         // allocate space for the return array
         movieInfo** retArr = movieInfo**[sizeA + sizeB];
