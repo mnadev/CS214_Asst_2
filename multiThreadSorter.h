2b89f9f12de236e3b2946246c3cf9bf9e3e3dbc4
Mohammad Nadeem <mohammednadeem902@gmail.com>
2018-11-13 13:02:33 -0500

    fixed mergesort definiton

--- multiThreadSorter.h
+++ multiThreadSorter.h
@@ -47,5 +47,5 @@ struct _movieInfo{
 
 
 //Suggestion: prototype a mergesort function
-void mergesort(movieInfo** arr, int leftInd, int rightInd, int isInt);                                                                                    
+void mergesort(movieInfo** arr, char * columnToBeSorted, int left, int right);                                                                                    
 
