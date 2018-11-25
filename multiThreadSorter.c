7fa8fcc9fdb4b2f3f60072f29ed7bcf81ab36a9e
Eric K. Lin <ekl40@ilab1.cs.rutgers.edu>
2018-11-25 14:24:16 -0500

    stashing changes

--- multiThreadSorter.c
+++ multiThreadSorter.c
@@ -1174,7 +1174,7 @@ int main(int argc, char** argv){
 		}
 		//TODO: write column names
 		char * columnNames = "color,director_name,num_critic_for_reviews,duration,director_facebook_likes,actor_3_facebook_likes,actor_2_name,actor_1_facebook_likes,gross,genres,actor_1_name,movie_title,num_voted_users,cast_total_facebook_likes,actor_3_name,facenumber_in_poster,plot_keywords,movie_imdb_link,num_user_for_reviews,language,country,content_rating,budget,title_year,actor_2_facebook_likes,imdb_score,aspect_ratio,movie_facebook_likes\n";
-		//csvwrite(head -> data, head -> arrLen, columnNames, fileToWrite);
+		csvwrite(head -> data, head -> arrLen, columnNames, fileToWrite);
 		free(fileToWrite); 
 	}
 	free(dirDest);
